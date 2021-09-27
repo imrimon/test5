7f03b0b199b1244e9970ce783eaa66cdb5bb9d15
imrimon <72396280+imrimon@users.noreply.github.com>
2021-09-27 22:25:08 +0600

    Commit done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header/Header';
+import Places from './Components/Places/Places';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Places></Places>
+    </div>
+  );
+}
+
+export default App;
